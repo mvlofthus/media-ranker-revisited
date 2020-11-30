0e958e485b50466688871995e26784cfc67e9a06
Mackenzie <mvlofthus@gmail.com>
2020-11-29 20:17:08 -0800

    oauth login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login
 
   def render_404
     return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
@@ -11,7 +12,19 @@ class ApplicationController < ActionController::Base
 
   def find_user
     if session[:user_id]
-      @login_user = User.find_by(id: session[:user_id])
+      @login_user = User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+    return @login_user
+  end
+
+  def require_login
+    if @login_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "Please log in to view this page"
+      redirect_to root_path
+      return
     end
   end
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+  validates :uid, uniqueness: {scope: :provider}
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = auth_hash["provider"]
+    user.username = auth_hash["info"]["nickname"]
+    user.email = auth_hash["info"]["email"]
+    user.avatar = auth_hash["info"]["image"]
+
+    return user
+  end
 end
